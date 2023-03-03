16b6094961e29808f19ab1ea9a5f0ac5f7474955
Mike Robles <mikej.robles@gmail.com>
2023-03-03 18:55:34 -0500

    Initials to name

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ function numberToString(num) {
   let numString = num.toString()
   return numString
 }
+
+// Daily code war challenge 3/3/2023
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+//The output should be two capital letters 
+
+
+function abbrevName(name){
+  
+  let nameString = name.split(" ")
+  let initials = ""
+  for (let i = 0; i < nameString.length; i++) {
+    if (nameString[i].length > 0 && nameString[i] !== "") {
+      initials += nameString[i][0]
+    }
+  }
+  return initials
+}
+
+console.log(abbrevName("Robert Frost"))
+
