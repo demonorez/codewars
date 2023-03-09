725f036ed2c06bfb90c3f2ab5ea38cbf4806c1a0
Mike Robles <mikej.robles@gmail.com>
2023-03-09 16:08:41 -0500

    daily code war challenge 3/9/2023

--- app.js
+++ app.js
@@ -66,3 +66,17 @@ function checkForFactor (base, factor) {
 
 
 console.log(checkForFactor(6, 3))
+
+// 3/9/2023 
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+function findNeedle(haystack) {
+  let needle = haystack.indexOf("needle")
+  return `found the needle at position ${needle}`
+}
