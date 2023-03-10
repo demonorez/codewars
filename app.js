d7a9e95c2d2ee7db51b5f16cb1c3947e9c6484f4
Mike Robles <mikej.robles@gmail.com>
2023-03-10 13:43:57 -0500

    challenge 3/10/2023

--- app.js
+++ app.js
@@ -80,3 +80,19 @@ function findNeedle(haystack) {
   let needle = haystack.indexOf("needle")
   return `found the needle at position ${needle}`
 }
+
+// 3/10/2023 
+// Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him
+
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message :)
+
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+function hoopCount (n) {
+  let hoopCounts = n
+  if (hoopCounts < 10) return "Keep at it until you get it."
+  if (hoopCounts >= 10) return "Great, now move on to tricks"
+}
+
+console.log(hoopCount(10));
