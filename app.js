b580a1116c80519665f9baec278dd515e09904c4
Mike Robles <mikej.robles@gmail.com>
2023-03-23 15:19:26 -0400

    3/23/2023

--- app.js
+++ app.js
@@ -259,3 +259,10 @@ function findShort(s){
   }, words[0])
   return shortest.length
 }
+
+// 3/23/2023
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+function reverseWords(str) {
+  return str.split('').reverse().join('').split(' ').reverse().join(' ')
+}
