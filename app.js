b17b670f13db8e5c13fae21bc9c5fc5d134d7a26
Mike Robles <mikej.robles@gmail.com>
2023-03-27 23:06:37 -0400

    3/27/2023

--- app.js
+++ app.js
@@ -266,3 +266,41 @@ function findShort(s){
 function reverseWords(str) {
   return str.split('').reverse().join('').split(' ').reverse().join(' ')
 }
+
+// Alt solution
+// function reverseWords(str) {
+//   return str.split(' ').map(function(word){
+//     return word.split('').reverse().join('');
+//   }).join(' ');
+// }
+
+// 3/27/2023
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+function friend(friends){
+  return friends.filter(n => n.length === 4)
+}
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a,b,c)
+{
+  return a + b > c && a + c > b && c + b > a;
+}
